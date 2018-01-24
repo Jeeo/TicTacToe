15312d67f3a1aa411d086d95db9fe429adeff43b
Jeeo <jeovano_oli@hotmail.com>
2018-01-24 16:50:16 -0300

    fix the minimax routine

--- js/ia.js
+++ js/ia.js
@@ -22,17 +22,19 @@ function genTree(depth, node) {
         n.board[i] = "X";
       }
       
-      if(verifyWinner(node.board)){
-        if(depth % 2 === 0) {
-          node.value = -1;
-        } else 
+      if( verifyWinner(node.board) ){
+        if(calcDepth(node) % 2 !== 0) {
           node.value = 1;
-         
+        } else {
+          node.value = -1;
+        }
         return;
       }
-      else
+      else{
         node.children.push(n);
         genTree(depth-1, n);
+      }
+        
     }
     else {
       continue;
@@ -58,43 +60,62 @@ function verifyWinner(board) {
   return false;
 }
 
-function miniMax(node, depth) {
-  
-  if(  isLeaf(node) || depth == 0 ) {
+function miniMax(node, depth =  calcDepth(node)) {
+
+  if(  isLeaf(node) || depth === 0 ) {
     return node.value;  
   }
 
-  if (depth % 2 !== 0) {
-    let a = Number.MAX_SAFE_INTEGER;
+  if (calcDepth(node) % 2 !== 0) {
+    let a = 2;
     node.children.forEach( element => {
-      a = Math.min(a, miniMax(element, depth-1))
-      return a;
+      // console.log("||[Min]depth: "+depth+" a: "+a +"||")
+      // console.log(element)
+      a = Math.min(a, miniMax(element, depth-1))  
+      // console.log("********Returning**********")
+      // console.log("[Min]depth: "+depth+" a: "+a)
     });  
+    return a;
   }
   else {
-    let a = Number.MIN_SAFE_INTEGER;
+    let a = -2;
     node.children.forEach( element => {
+      // console.log("||[Max]depth: "+depth+" a: "+a +"||")
+      // console.log(element)
       a = Math.max(a, miniMax(element, depth-1))
-      console.log(a)
-      return a;
+      // console.log("********Returning**********")
+      // console.log("[Max]depth: "+depth+" a: "+a)
     })
+    return a;
   }
-
 }
 
 function isLeaf(node) {
-  return (node.children.lenght == 0) ? true : false;
+  return (node.children.length === 0) ? true : false;
 }
 
 
 function searchTest(node, depth) {
-  if(isLeaf(node) || depth == 0) return 
-  node.children.forEach( element => { console.log(element.board)
-    searchTest(element, depth-1);
+  if( (isLeaf(node) || depth == 0) && node.value === 0 ) {
+    console.log(node)
+    return;
+  } 
     
+  node.children.forEach( element => {
+    searchTest(element, depth-1);
   })
   return
 }
+
+function calcDepth(node) {
+  let qnt = 0;
+  for(i=0; i<9;i++){
+    if (node.board[i])
+      qnt++; 
+  }
+  return 9-qnt;
+}
+
 let a = new tNode;
 
 genTree(9, a);
